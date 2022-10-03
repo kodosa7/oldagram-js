1a2d7cad52a81532ca4c9033c31e796c7d45fd50
ELS <57393100+kodosa7@users.noreply.github.com>
2022-10-03 13:03:40 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const posts = [
+    {
+        name: "Vincent van Gogh",
+        username: "vincey1853",
+        location: "Zundert, Netherlands",
+        avatar: "images/avatar-vangogh.jpg",
+        post: "images/post-vangogh.jpg",
+        comment: "just took a few mushrooms lol",
+        likes: 21
+    },
+    {
+        name: "Gustave Courbet",
+        username: "gus1819",
+        location: "Ornans, France",
+        avatar: "images/avatar-courbet.jpg",
+        post: "images/post-courbet.jpg",
+        comment: "i'm feelin a bit stressed tbh",
+        likes: 4
+    },
+        {
+        name: "Joseph Ducreux",
+        username: "jd1735",
+        location: "Paris, France",
+        avatar: "images/avatar-ducreux.jpg",
+        post: "images/post-ducreux.jpg",
+        comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
+        likes: 152
+    }
+]
+
+const postImgEl = document.getElementById("post-img-el")
+
+function renderImg() {
+    let imgPath = posts[0].post
+    console.log(imgPath)
+    const imgDOM = `<img id="post-img-el" src="${imgPath}">`
+    postImgEl.innerHTML = imgDOM
+}
+
+renderImg()
